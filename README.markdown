e8507bd385c42e8a108b8622a62f343916ad971b
jaysphoto <jay@jayhaabee.nl>
2021-10-12 11:05:01 +0200

    Added build badge Travis-ci.com

---
+++ README.markdown
@@ -0,0 +1,10 @@
+# Puppet Pritunl Module
+[![Build Status](https://app.travis-ci.com/jaysphoto/puppet-pritunl.svg?branch=master)](https://app.travis-ci.com/jaysphoto/puppet-pritunl)
+
+## Overview
+This will setup the official pritunl repository and install the main pritunl package
+
+Supports:
+* CentOS
+* Ubuntu
+
--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -4,11 +4,11 @@ class pritunl::ubuntu {
 #    server => 'keyserver.ubuntu.com'
 #  }
   apt::source { 'pritunl':
-    location  => 'https://repo.pritunl.com/stable/apt',
-    repos     => 'main',
-    key => {
-      id      => '7568D9BB55FF9E5287D586017AE645C0CF8E292A',
-      server  => 'keyserver.ubuntu.com'
+    location => 'https://repo.pritunl.com/stable/apt',
+    repos    => 'main',
+    key      => {
+      id     => '7568D9BB55FF9E5287D586017AE645C0CF8E292A',
+      server => 'keyserver.ubuntu.com'
     }
   }
 
